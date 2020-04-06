f3ca8a23c3a3df20d684ff49732d8a58b793c3f5
kjcblack <kjara@miuandes.cl>
2020-04-06 03:52:03 -0400

    Failed terminal

---
+++ db/schema.rb
@@ -0,0 +1,76 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_04_06_053406) do
+
+  create_table "customers", force: :cascade do |t|
+    t.string "name"
+    t.string "last_name"
+    t.string "email"
+    t.integer "phone"
+    t.string "password"
+    t.string "address"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "event_venues", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.integer "capacity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "events", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.date "star_date"
+    t.integer "event_venue_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_venue_id"], name: "index_events_on_event_venue_id"
+  end
+
+  create_table "order_tickets", force: :cascade do |t|
+    t.integer "ticket_type_id", null: false
+    t.integer "quantity"
+    t.integer "order_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["order_id"], name: "index_order_tickets_on_order_id"
+    t.index ["ticket_type_id"], name: "index_order_tickets_on_ticket_type_id"
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "customer_id", null: false
+    t.integer "number_order"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+  end
+
+  create_table "tickets", force: :cascade do |t|
+    t.string "name"
+    t.integer "event_id", null: false
+    t.integer "price"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["event_id"], name: "index_tickets_on_event_id"
+  end
+
+  add_foreign_key "events", "event_venues"
+  add_foreign_key "order_tickets", "orders"
+  add_foreign_key "order_tickets", "ticket_types"
+  add_foreign_key "orders", "customers"
+  add_foreign_key "tickets", "events"
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+customer1 =  Customer.create(name:"Emilia", last_name: "Lopez", email:"emilia@gmail.com", phone: 7893456, password: "moon", address:"Santiago 567")
+customer2 = Customer.create(name:"Eduardo", last_name: "Solis", email:"eduardo@gmail.com", phone: 7893456, password: "winter", address:"Santiago 987")
+
+
+
+event_venue1 = EventVenue.create(name: "Park", address: "Pucon 14", capacity:500)
+event_venue2 = EventVenue.create(name: "Snow", address: "Punta Arenas 714", capacity:600)
+event_venue3 = EventVenue.create(name: "Sky", address: "Santiago 98", capacity:100)
+
+event1 = Event.create(name: "Moon" ,description: "empty" , star_date: Date.new(2020,5,12) ,event_venue:event_venue1)
+event2 = Event.create(name:"Winter" ,description: "empty" , star_date: Date.new(2021,6,17) ,event_venue:event_venue2)
+event3 =Event.create(name:"Sunset" ,description: "empty" , star_date:Date.new(2020,5,12) ,event_venue:event_venue3)
+
+
+order1 = Order.create(customer:customer1, number_order: 1)
+order2 = Order.create(customer:customer2, number_order: 2)
+order3 = Order.create(customer:customer1, number_order: 3)
+order4 = Order.create(customer:customer2, number_order: 4)
+
+ticket1 = Ticket.create(name: "gold", event:event1, price:5000)
+ticket2 = Ticket.create(name: "general", event:event1, price:500)
+ticket3 = Ticket.create(name: "gold", event:event2, price:5000)
+ticket4 = Ticket.create(name: "general", event:event2, price:500)
+ticket5 = Ticket.create(name: "gold", event:event3, price:5000)
+ticket6 = Ticket.create(name: "general", event:event3, price:500)
